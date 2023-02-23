34dad4fc80c0c5c23654a6e1e16d74b555ae2e46
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2023-02-23 14:08:08 +1300

    Introduce initial HTTP Webhook action

---
+++ src/Azure.LogicApps.NET/Actions/HttpWebhook.cs
@@ -0,0 +1,28 @@
+﻿using Azure.LogicApps.NET.Base;
+
+namespace Azure.LogicApps.NET.Actions;
+
+public class HttpWebhook : WorkflowActionBase
+{
+	public HttpWebhookInput Inputs { get; set; }
+
+	public class HttpWebhookInput
+	{
+		public HttpWebhookSubscription Subscribe { get; set; } = new();
+
+		public HttpWebhookSubscription Unsubscribe { get; set; }
+	}
+
+	public class HttpWebhookSubscription
+	{
+		public string Method { get; set; }
+
+		public Uri Uri { get; set; }
+
+        public Dictionary<string, string> Headers { get; set; }
+
+        public object Body { get; set; }
+
+		public object Authentication { get; set; }
+	}
+}
---
+++ tests/Azure.LogicApps.NET.Tests/Actions/HttpWebhookTests.cs
@@ -0,0 +1,57 @@
+﻿using Azure.LogicApps.NET.Actions;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+
+namespace Azure.LogicApps.NET.Tests.Actions;
+
+public class HttpWebhookTests
+{
+	[Fact]
+	public void ToWorkflowJsonString_PopulatesCorrectionJson()
+	{
+		HttpWebhook action = new HttpWebhook
+		{
+			ActionIdentifier = "HttpWebhook1",
+			Inputs = new HttpWebhook.HttpWebhookInput
+			{
+				Subscribe = new HttpWebhook.HttpWebhookSubscription
+				{
+					Method = "POST",
+					Uri = new Uri("http://www.xyz.com"),
+					Body = new
+					{
+						Person = "@variables('person')",
+						CallbackUrl = "@listCallbackUrl()"
+					}
+				}
+			}
+		};
+
+		var actualJsonString = action.ToWorkflowJsonString();
+		var actualJObject = JObject.Parse(actualJsonString);
+
+		string expectedJsonString =
+			"""
+            {
+              "type": "HttpWebhook",
+              "inputs": {
+                "subscribe": {
+                  "method": "POST",
+                  "uri": "http://www.xyz.com",
+                  "headers": null,
+                  "body": {
+                    "person": "@variables(\u0027person\u0027)",
+                    "callbackUrl": "@listCallbackUrl()"
+                  },
+                  "authentication": null
+                },
+                "unsubscribe": null
+              },
+              "runAfter": {}
+            }
+            """;
+		var expectedJObject = JObject.Parse(expectedJsonString);
+
+		JToken.DeepEquals(actualJObject, expectedJObject).Should().BeTrue();
+	}
+}
